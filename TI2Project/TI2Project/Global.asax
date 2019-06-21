203be88eb9701ad32d78ac4849bfb0120748f7d1
Vasco Marçal <vascohenriquemarcal@gmail.com>
2019-06-21 13:21:00 +0100

    teste

---
+++ TI2Project/TI2Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TI2Project.MvcApplication" Language="C#" %>
---
+++ TI2Project/TI2Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TI2Project.Startup))]
+namespace TI2Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
