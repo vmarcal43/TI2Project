c2c57c51ff5f80232cc9cb3c2b97e4d276aeedc7
Vasco Marçal <vascohenriquemarcal@gmail.com>
2019-07-08 17:36:36 +0100

    users

--- TI2Project/TI2Project/Startup.cs
+++ TI2Project/TI2Project/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using TI2Project.Models;
 
 [assembly: OwinStartupAttribute(typeof(TI2Project.Startup))]
 namespace TI2Project
@@ -9,6 +12,52 @@ namespace TI2Project
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            addUsers();
+        }
+
+        /// <summary>
+        /// criar os utilizadores no sistema
+        /// </summary>
+        private void addUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new
+            RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new
+            UserStore<ApplicationUser>(context));
+            // criaçao da role admin e das contas dos administradores
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                // criaçao da role admin
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+
+                role.Name = "Admin";
+                roleManager.Create(role);
+                //criaçao do admin vasco
+                var userVM = new ApplicationUser();
+                userVM.UserName = "vasco@web.com";
+                userVM.Email = "vasco@web.com";
+                string userVMPWD = "aA123.";
+                var chkUserVM = userManager.Create(userVM, userVMPWD);
+                //Add default User to Role Admin
+                if (chkUserVM.Succeeded)
+                {
+                    var result1 = userManager.AddToRole(userVM.Id, "Admin");
+                }
+
+                //criaçao do admin henrique
+                var userH = new ApplicationUser();
+                userH.UserName = "henrique@web.com";
+                userH.Email = "henrique@web.com";
+                string userHPWD = "aA123.";
+                var chkUserH = userManager.Create(userH, userHPWD);
+                //Add default User to Role Admin
+                if (chkUserH.Succeeded)
+                {
+                    var result1 = userManager.AddToRole(userH.Id, "Admin");
+                }
+            }
         }
     }
 }
